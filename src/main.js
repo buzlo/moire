ed04b010125332f99b77f5cf2eaba58b30bd13c3
Dmitry <dm.buzaev@mail.ru>
2023-05-14 23:22:12 +0300

    global styles added

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import { createPinia } from 'pinia'
 import router from './pages/routes'
-import './style.css'
+import './style.scss'
 
 const store = createPinia();
 createApp(App).use(router).use(store).mount('#app')
