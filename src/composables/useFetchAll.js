27756dfd460a657862b926c102d6a73891026139
Dmitry <dm.buzaev@mail.ru>
2023-05-16 21:39:14 +0300

    useFetchAll optimised, content scss added

--- src/composables/useFetchAll.js
+++ src/composables/useFetchAll.js
@@ -1,6 +1,6 @@
 export async function useFetchAll(urls, propertyToGet = null) {
   return await Promise.all(
-    urls.map((url) => fetch(url).then(async (response) => await response.json()))
+    urls.map((url) => fetch(url).then(response => response.json()))
   ).then((parsedResponses) =>
     propertyToGet ? parsedResponses.map((parsedResponse) => parsedResponse[propertyToGet]) : parsedResponses
   )
